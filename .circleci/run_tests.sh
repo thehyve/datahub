deccd6a43a539c786659b98f9c06961fac873923
Pieter Lukasse <pieterlukasse@gmail.com>
2018-02-16 14:58:32 +0100

    fix move2

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -40,7 +40,7 @@ if [[ $num_studies > 0 ]]; then
   sh -c "$validation_command"
   
   # move errors to ERRORS/ folder:
-  erred_studies=`grep -rnlz '.' -e 'Validation status.*Failed' $test_reports_location `
+  erred_studies=`grep -rnlz $test_reports_location -e 'Validation status.*Failed' `
   sh -c "mv $erred_studies $test_reports_location/ERRORS"
 
 else
